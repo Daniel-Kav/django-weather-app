d668a645e828f6fbca3c2b8c8dbe087e31f770d0
daniel-kav <dankavatha01@gmail.com>
2025-02-18 21:08:46 +0300

    weather app working

--- weather_app/weather/views.py
+++ weather_app/weather/views.py
@@ -3,30 +3,61 @@ from django.conf import settings
 from django.shortcuts import render
 from .models import WeatherData
 
+# def get_weather_data(city):
+#     api_key = settings.OPENWEATHERMAP_API_KEY
+#     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+#     response = requests.get(url)
+#     return response.json()
+
 def get_weather_data(city):
     api_key = settings.OPENWEATHERMAP_API_KEY
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
     response = requests.get(url)
-    return response.json()
+    data = response.json()
+    if data.get('cod') == 200:
+        return {
+            'city': data['name'],
+            'temperature': data['main']['temp'],
+            'description': data['weather'][0]['description'],
+            'icon': data['weather'][0]['icon'],  # Add weather icon
+        }
+    return None
+
+# def weather_view(request):
+#     if request.method == 'POST':
+#         city = request.POST.get('city')
+#         data = get_weather_data(city)
+#         if data.get('cod') == 200:
+#             weather_data = {
+#                 'city': data['name'],
+#                 'temperature': data['main']['temp'],
+#                 'description': data['weather'][0]['description'],
+#             }
+#             # Save to database (optional)
+#             WeatherData.objects.create(
+#                 city=weather_data['city'],
+#                 temperature=weather_data['temperature'],
+#                 description=weather_data['description']
+#             )
+#             return render(request, 'weather/weather.html', {'weather': weather_data})
+#         else:
+#             error_message = data.get('message', 'Error fetching weather data.')
+#             return render(request, 'weather/weather.html', {'error': error_message})
+#     return render(request, 'weather/weather.html')
 
 def weather_view(request):
     if request.method == 'POST':
         city = request.POST.get('city')
         data = get_weather_data(city)
-        if data.get('cod') == 200:
+        if data:
             weather_data = {
-                'city': data['name'],
-                'temperature': data['main']['temp'],
-                'description': data['weather'][0]['description'],
+                'city': data['city'],
+                'temperature': data['temperature'],
+                'description': data['description'],
+                'icon': data['icon'],  # Pass icon to template
             }
-            # Save to database (optional)
-            WeatherData.objects.create(
-                city=weather_data['city'],
-                temperature=weather_data['temperature'],
-                description=weather_data['description']
-            )
             return render(request, 'weather/weather.html', {'weather': weather_data})
         else:
-            error_message = data.get('message', 'Error fetching weather data.')
+            error_message = "City not found. Please try again."
             return render(request, 'weather/weather.html', {'error': error_message})
     return render(request, 'weather/weather.html')
